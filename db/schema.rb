23fd4fd14efdf5f98f2ccf6cef93fd05541be46b
Liza <liza@qburst.com>
2022-06-30 23:58:19 +0530

    Remove User scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_30_172513) do
+ActiveRecord::Schema.define(version: 2022_06_29_074133) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -27,12 +26,6 @@ ActiveRecord::Schema.define(version: 2022_06_30_172513) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
